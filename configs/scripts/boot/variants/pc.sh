88d91a6f7ee6573259e9473f0822bac1553faa16
Hagen <78751562+H4K0N42@users.noreply.github.com>
2026-02-28 00:55:33 +0100

    Update pc.sh

--- configs/scripts/boot/variants/pc.sh
+++ configs/scripts/boot/variants/pc.sh
@@ -5,4 +5,4 @@ shopt -s extglob # Match !(...)
 cd /home/schule/ && rm -rf !(Documents)
 cp -r /etc/nixos/git-config/configs/dotfiles/pc/config/ /home/schule/.config/
 
-bash /etc/nixos/git-config/configs/scripts/update.sh
+bash "/etc/nixos/git-config/configs/scripts/update.sh"
