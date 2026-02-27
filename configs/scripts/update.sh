af8320c7d829be148809ad1e4aaf151e8b94fb6c
Hagen <78751562+H4K0N42@users.noreply.github.com>
2026-02-27 23:46:57 +0100

    Update update.sh

--- configs/scripts/update.sh
+++ configs/scripts/update.sh
@@ -3,11 +3,6 @@
 REPO_URL="https://github.com/H4K0N42/LGY-NixOS"
 TARGET_DIR="/etc/nixos/git-config"
 
-if [ "$EUID" -ne 0 ]; then
-    echo "This script must be run as root or with sudo."
-    exit 1
-fi
-
 rm -rf $TARGET_DIR
 git clone --depth 1 $REPO_URL $TARGET_DIR
 
