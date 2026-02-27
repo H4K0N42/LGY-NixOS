138705cb3ce45eae0807cf34a0080aea13e2db86
Hagen <78751562+H4K0N42@users.noreply.github.com>
2026-02-28 00:40:23 +0100

    Update update.sh

--- configs/scripts/update.sh
+++ configs/scripts/update.sh
@@ -1,11 +1,12 @@
 #!/run/current-system/sw/bin/bash
 
-REPO_URL="https://github.com/H4K0N42/LGY-NixOS"
 TARGET_DIR="/etc/nixos/git-config"
 
-cd /tmp/
-rm -rf $TARGET_DIR
-git clone --depth 1 $REPO_URL $TARGET_DIR
+cd $TARGET_DIR
+git fetch origin
+git reset --hard origin/main
+git clean -fd
+
 
 cp -f "$TARGET_DIR/flake.nix" "/etc/nixos/flake.nix"
 rm -f /etc/nixos/flake.lock
