ce087b48802c83087857df224f35296cc72970f3
Hagen <78751562+H4K0N42@users.noreply.github.com>
2026-03-01 21:53:37 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,13 @@ TARGET_DIR="/etc/nixos/git-config"
 REPO_URL="https://github.com/H4K0N42/LGY-NixOS/"  # Change this to your repo
 CONFIG_FILE="/etc/nixos/configuration.nix"
 
-NEW_HOSTNAME="$1"
-
 mkdir -p "$TARGET_DIR"
 git clone "$REPO_URL" "$TARGET_DIR"
 
 cp -f "$TARGET_DIR/flake.nix" "/etc/nixos/flake.nix"
 rm -f /etc/nixos/flake.lock
+
+read -rp "Hostname> " NEW_HOSTNAME
 
 echo "$NEW_HOSTNAME" > /etc/nixos/hostname
 
