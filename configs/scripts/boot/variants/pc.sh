8863b9849c70cf387c8ba04564d8da6502305393
Hagen <H4K0N@gmx.net>
2026-05-16 00:30:18 +0200

    Update pc.sh

--- configs/scripts/boot/variants/pc.sh
+++ configs/scripts/boot/variants/pc.sh
@@ -1,7 +1,6 @@
 #!/run/current-system/sw/bin/bash
 
-shopt -s extglob # Match !(...)
+find /home/schule -mindepth 1 -maxdepth 1 ! -name Documents -delete
 
-cd /home/schule/ && rm -rf !(Documents)
 rsync -r /etc/nixos/git-config/configs/dotfiles/config/ /home/schule/.config/
 chown -R schule:users /home/schule/
