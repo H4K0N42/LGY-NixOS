4e24e17b567dd86335fad5a76ae87048f83b7cce
Hagen <H4K0N@gmx.net>
2026-05-21 10:29:53 +0200

    Change delete command to use rm -rf with exec

--- configs/scripts/boot/variants/note.sh
+++ configs/scripts/boot/variants/note.sh
@@ -1,6 +1,6 @@
 #!/run/current-system/sw/bin/bash
 
-find /home/schule -mindepth 1 -maxdepth 1 ! -name Documents -delete
+find /home/schule -mindepth 1 -maxdepth 1 ! -name Documents -exec rm -rf {} +
 
 rsync -r /etc/nixos/git-config/configs/dotfiles/config/ /home/schule/.config/
 chown -R schule:users /home/schule/
