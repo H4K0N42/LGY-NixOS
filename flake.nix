8330823c70635f63b9628baf2486a8a3058bd2a0
Hagen <78751562+H4K0N42@users.noreply.github.com>
2026-06-15 20:07:52 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,10 +2,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
-    veyon = {
-      url = "github:veyon/veyon";
-      submodules = true;
-    };
+    veyon.url = "github:veyon/veyon?submodules=1";
   };
 
   outputs =
