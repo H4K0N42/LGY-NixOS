312f2255d1205061fc7cc5e492e2b2aad3fbad70
Hagen <78751562+H4K0N42@users.noreply.github.com>
2026-06-15 19:46:04 +0200

    26.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
     veyon.url = "github:veyon/veyon";
   };
