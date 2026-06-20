8e644024be626587f25f7b13f4e7209149aa021c
Hagen <78751562+H4K0N42@users.noreply.github.com>
2026-06-20 17:07:43 +0200

    Update default.nix

--- configs/default.nix
+++ configs/default.nix
@@ -52,7 +52,20 @@
     publicKey = {
       name = "LGY-NixOS";
       value = ''
-
+        -----BEGIN PUBLIC KEY-----
+        MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAyDNqvh1Yuw2T6Z2u0MYc
+        xcaCQuQYTyslIjKN5w6VeCKnmMzhG+pCEB4ka/JdRp2rf5pl+6ZK1hvK+inL2ngn
+        FtZA48pu3fXa0D92VBZsZgsbDBqfJ6l9tK1tBvCrEw3wcA3Lx7MYCkR5rbShC5M2
+        ZjntGKmkmhTFq9xRAWc0oTLIqrjwPN67jpSLCvnAf+N8YUanzDzud13M+nn0JkAi
+        XfMybnHkqp09R1n6w0IBB1hXunJgMu2fdZ6e0fzQVP0KfrB2JOyB89lAXUw40VAV
+        27MF/bFcCOE2skZu1dazB8TvjlmAPjLhKhJZr1ERJ2H2Fhrj4x5V2zXC0bqkEyXa
+        kaH0YnSEn8idtGdeKbSkALG67tYDdfS1g8vrs/4d5xi4nPWp1c+tQtX842lDiQ+8
+        GsROOA1jUgFkCPmljxIuG7wyetv6iOhRzpUWKndkuIJYBJzDKca1X8oPpkmgduWz
+        OBGYrutUVdfIImPLrWUFpra+2I48Dhd1u2wPLmjTmPnEeDfePWjpxrEQNleN6EQi
+        CBnmUvkw6kv2urfAVxMz6ySnn8uui7/NniXXWL0OSdhBt3kQ3gHdi4mnYH7jb65i
+        +va0xmgMwDdACzS9kQdMGLiXp3kLQ9QZrfjg3Ka45UC/ioDRcoQWOin6/GCkjkBk
+        0petrU/JDBLpZNd+xb+oXu0CAwEAAQ==
+        -----END PUBLIC KEY-----
       '';
     };
   };
